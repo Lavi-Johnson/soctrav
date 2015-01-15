8840fe2cefcb1a77905e3cf96bade09cdf583de3
Yanni Peng <yanni.peng@vocalocity.com>
2015-01-15 10:51:07 -0500

    implements UserDetailsService to make spring security work

--- src/main/java/com/yanni/sotrav/services/User/BaseUserService.java
+++ src/main/java/com/yanni/sotrav/services/User/BaseUserService.java
@@ -1,16 +1,19 @@
 package com.yanni.sotrav.services.User;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import com.yanni.sotrav.manager.UserManager;
 import com.yanni.sotrav.models.User;
 
 @Component("baseUserService")
-public class BaseUserService {
+public class BaseUserService implements org.springframework.security.core.userdetails.UserDetailsService{
 	
 	@Autowired
 	@Qualifier("userManagerBean")
@@ -30,6 +33,17 @@ public class BaseUserService {
 	
 	public void delete(User usr){
 		_userManager.delete(usr);
+	}
+	
+	public List<User> findAll(){
+		return _userManager.findAll();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return find(username);
 	}
 
 }
