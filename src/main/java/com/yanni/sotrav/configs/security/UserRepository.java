2a0767d105a6f52a285941634623720523c593b5
Yanni Peng <yanni.peng@vocalocity.com>
2015-01-16 15:17:20 -0500

    comment out abstract method findByUsername causing problems n not really using this interface

--- src/main/java/com/yanni/sotrav/configs/security/UserRepository.java
+++ src/main/java/com/yanni/sotrav/configs/security/UserRepository.java
@@ -1,12 +1,10 @@
 package com.yanni.sotrav.configs.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import com.yanni.sotrav.models.User;
 
+//not working!!!!!
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+	//User findByUsername(String username);
 }
