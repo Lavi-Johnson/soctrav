72ee023893f18728748b9ac19b71926cd9002755
Yanni Peng <yanni.peng@vocalocity.com>
2015-01-16 21:14:30 -0500

    nothing changed here

--- src/main/java/com/yanni/sotrav/configs/security/StatelessAuthenticationFilter.java
+++ src/main/java/com/yanni/sotrav/configs/security/StatelessAuthenticationFilter.java
@@ -7,7 +7,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -24,8 +26,7 @@ class StatelessAuthenticationFilter extends GenericFilterBean {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
 			ServletException {
-		SecurityContextHolder.getContext().setAuthentication(
-				tokenAuthenticationService.getAuthentication((HttpServletRequest) req));
+		SecurityContextHolder.getContext().setAuthentication(tokenAuthenticationService.getAuthentication((HttpServletRequest) req));
 		chain.doFilter(req, res); // always continue
 	}
 }
