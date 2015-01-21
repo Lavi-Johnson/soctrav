cfc3f5e27379c0cd2b8472e86c3aeebf6a634121
Yanni Peng <yanni.peng@vocalocity.com>
2015-01-21 01:53:21 -0500

    main class

---
+++ src/main/java/com/yanni/sotrav/Application.java
@@ -0,0 +1,41 @@
+package com.yanni.sotrav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
+	
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+  }
+
+  private static Class<Application> applicationClass = Application.class;
+
+  public static void main(String[] args) {
+	//http://localhost:8080/soctrav/test
+    SpringApplication.run(applicationClass, args);
+  }
+  
+  @Bean
+  public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/forbidden.html"));
+            }
+   };
+}
+}
