4c6f1c9c832b6e181b741505650699cc1856f581
Yanni Peng <yanni.peng@vocalocity.com>
2015-04-20 00:10:51 -0400

    hard coded constants used throughout this project is in SharedConstants class. made a forbidden jsp page for 403. sideBarAdmin.jsp has the navigation for admin pages

---
+++ src/main/java/com/yanni/sotrav/common/SharedConstants.java
@@ -0,0 +1,12 @@
+package com.yanni.sotrav.common;
+
+public class SharedConstants {
+	
+	public static final short SUPER_TYPE = 1;
+	public static final short ADMIN_TYPE = 2;
+	
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    public static final String USER_LOGIN="userLogin";
+
+}
