53988ac2c9163df15c1a5a005cbbaf9ad09ff0da
Yanni Peng <yanni.peng@vocalocity.com>
2015-05-28 10:46:33 -0400

    added locations to the modelmap on user.html

--- src/main/java/com/yanni/sotrav/controllers/AdminNavController.java
+++ src/main/java/com/yanni/sotrav/controllers/AdminNavController.java
@@ -1,17 +1,24 @@
 package com.yanni.sotrav.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yanni.sotrav.common.ApplicationBeanFactory;
 import com.yanni.sotrav.common.SharedConstants;
+import com.yanni.sotrav.models.Location;
 import com.yanni.sotrav.models.User;
+import com.yanni.sotrav.services.location.ILocationService;
 
 /**
  * Class UserController
@@ -21,6 +28,9 @@ public class AdminNavController {
 	
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(AdminNavController.class);
+	
+	@Autowired
+	ApplicationBeanFactory factorybean;
 	
 	@RequestMapping("admin/room.html")
 	public ModelAndView adminRoom(HttpServletRequest request) {
@@ -33,10 +43,13 @@ public class AdminNavController {
 	
 	@RequestMapping("admin/user.html")
 	public ModelAndView adminUser(HttpServletRequest request) {
+		ILocationService locServ=(ILocationService) factorybean.getBean("locationService");
 		LOGGER.info("is this a admin???"+request.isUserInRole("ROLE_"+SharedConstants.ADMIN.toString()));
 		ModelMap modelMap=new ModelMap();
 		User usr=(User)request.getSession().getAttribute("userLogin");
+		List<Location> locs=locServ.getUserLocations(usr);
 		modelMap.put("usrName", usr.getUser_email());
+		modelMap.put("locations", locs);
 		return new ModelAndView("user", modelMap);
 	}
 	
